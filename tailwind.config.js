645fdea225cec6513e752d53e3f223f2e329b938
Miguel <miguelgomez1133@gmail.com>
2025-07-05 11:56:22 -0300

    cambios en videoroom

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        // La duración y la función de temporización se mantienen
+        'pulse-custom': 'pulse-custom 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      keyframes: {
+        'pulse-custom': {
+          '0%': { opacity: '1', transform: 'scale(1)' },
+          '50%': { opacity: '0.5', transform: 'scale(0.8)' }, // Escala a 80% y reduce opacidad
+          '100%': { opacity: '1', transform: 'scale(1)' },
+        }
+      }
+    },
   },
   plugins: [],
 };
