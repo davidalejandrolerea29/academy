9a3cfaef1ce2c425120b7873606a26453572df13
Miguel <miguelgomez1133@gmail.com>
2025-07-22 20:32:35 -0300

    cambios en twilio

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_TWILIO_ACCOUNT_SID: string;
+  readonly VITE_TWILIO_AUTH_TOKEN: string;
+  // Agrega aquí cualquier otra variable de entorno que uses con VITE_
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
